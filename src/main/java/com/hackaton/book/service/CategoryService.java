113c227a90deaee8e0c44d8a27d416e66784cc57
herizoran <herizo.ran@gmail.com>
2022-08-26 11:53:42 +0300

    Create CategoryService.java

---
+++ src/main/java/com/hackaton/book/service/CategoryService.java
@@ -0,0 +1,61 @@
+package com.hackaton.book.service;
+
+import com.hackaton.book.model.Category;
+import com.hackaton.book.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+    private CategoryRepository repository;
+
+    //GET MAPPING
+    public List<Category> allCategory(){
+        return repository.findAll();
+    }
+    public List<Category> getCategoryByName(String categoryName){
+        return repository.findByNameCategoryContainingIgnoreCase(categoryName);
+    }
+    public Optional<Category> getCategoryById(Long id){
+        return repository.findById(id);
+    }
+
+    //POST MAPPING
+    public void insertCategory(Category category){
+        repository.save(category);
+    }
+
+    //DELETE MAPPING
+    public void deleteById(Long id){
+        repository.deleteById(id);
+    }
+
+    //PUT MAPPING
+    public void putUpdateCategory(Long id, Category newCategory){
+        List<Category> categories = allCategory();
+        for(Category categoryInfo : categories){
+            if(categoryInfo.getIdCategory() == id){
+                categoryInfo.setNameCategory(newCategory.getNameCategory());
+                repository.save(categoryInfo);
+            }else{
+            throw new NullPointerException("sorry you have a problem in:");
+            }
+        }
+
+    }
+
+    //PATCH MAPPING
+    public void patchUpdateCategory(Long id, Category newCategory){
+        List<Category> categories = allCategory();
+        for(Category categoryInfo : categories){
+            if(categoryInfo.getIdCategory() == id){
+                categoryInfo.setNameCategory(newCategory.getNameCategory());
+                repository.save(categoryInfo);
+            }
+        }
+    }
+}
