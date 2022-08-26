69443ed7edffb8a2a9c06ddfd423ffa5c4d1d442
herizoran <herizo.ran@gmail.com>
2022-08-26 14:55:03 +0300

    feat: made it serializable

--- src/main/java/com/hackaton/book/model/Category.java
+++ src/main/java/com/hackaton/book/model/Category.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Category {
+public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCategory;
