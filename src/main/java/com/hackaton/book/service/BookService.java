5f6e36d9532885351cb825d3d1aaa1e6fcf47f27
haritiana adriano <hei.adriano.4@gmail.com>
2022-08-26 13:07:55 +0300

    fix: book error

--- src/main/java/com/hackaton/book/service/BookService.java
+++ src/main/java/com/hackaton/book/service/BookService.java
@@ -3,11 +3,10 @@ package com.hackaton.book.service;
 import com.hackaton.book.model.Book;
 import com.hackaton.book.repository.BookRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,11 +15,10 @@ import java.util.Optional;
 public class BookService {
     private BookRepository bookRepository;
 
-    public List<Book> getAllBooks(Long page, Long pageSize){
-        if(page != null && pageSize != null){
-            Pageable pageable = PageRequest.of(Math.toIntExact(page), Math.toIntExact(pageSize));
-            return (List<Book>) bookRepository.findAll(pageable);
-        }else {
+    public List<Book> getAllBooks(int page, int pageSize){
+        if(page != 0 && pageSize != 0){
+            return (List<Book>) bookRepository.findAll(PageRequest.of(page, pageSize));
+        } else {
             return bookRepository.findAll();
         }
     }
@@ -35,5 +33,36 @@ public class BookService {
 
     public void insertBook(Book book){
         bookRepository.save(book);
+    }
+
+    public void updateBook(Long id ,Book newBook){
+        Optional<Book> thisBook = bookRepository.findByIdBook(id);
+        thisBook.get().setTitle(newBook.getTitle());
+        thisBook.get().setCategory(newBook.getCategory());
+        thisBook.get().setAuthor(newBook.getAuthor());
+        thisBook.get().setSynopsis(newBook.getSynopsis());
+        thisBook.get().setPages(newBook.getPages());
+    }
+
+    public void deleteBookById(Long id){
+        bookRepository.deleteById(id);
+    }
+
+    public int bookCount(){
+        List<Book> allBooks = bookRepository.findAll();
+        int bookCount = allBooks.size();
+        return bookCount;
+    }
+
+    public List<Book> bookByCategory(Long id){
+        List<Book> allBooks = bookRepository.findAll();
+        List<Book> allBookByCategory = new ArrayList<>();
+
+        for(Book book : allBooks){
+            if(book.getCategory().getIdCategory() == id){
+                allBookByCategory.add(book);
+            }
+        }
+        return allBookByCategory;
     }
 }
